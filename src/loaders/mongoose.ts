56068a971c5e4917da36cea2be0dc3ff6d6a0668
hoangnguyennn <nvanhoang188@gmail.com>
2022-11-23 15:02:56 +0700

    config mongoose debug

--- src/loaders/mongoose.ts
+++ src/loaders/mongoose.ts
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
 import env from '~/configs/env';
 
+const isDev = process.env.NODE !== 'production';
+
 export default async () => {
   await mongoose.connect(env.mongoUri);
+  mongoose.set('debug', isDev);
   console.log('Connected to MongoDB');
 };
